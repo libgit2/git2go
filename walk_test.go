56acff247ba6997c43292a70cf1ec01145e3aeec
Carlos Martín Nieto <cmn@dwim.me>
2013-09-13 10:57:34 +0200

    Do a basic test of the walker

---
+++ walk_test.go
@@ -0,0 +1,23 @@
+package git
+
+import (
+	"os"
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+	repo := createTestRepo(t)
+	defer os.RemoveAll(repo.Workdir())
+	commitId, _ := seedTestRepo(t, repo)
+
+	walk, err := repo.Walk()
+	checkFatal(t, err)
+	walk.Push(commitId)
+	walk.Sorting(SortTime | SortReverse)
+	var id Oid
+	err = walk.Next(&id)
+	checkFatal(t, err)
+	if id.Cmp(commitId) != 0 {
+		t.Fatal("Bad id returned")
+	}
+}
