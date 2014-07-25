1e651aaa22a35bbcaaa1af4da0e05a674833ebdc
David Calavera <david.calavera@gmail.com>
2014-07-24 17:08:58 -0700

    Add simple test for `rev-parse HEAD`.

--- revparse_test.go
+++ revparse_test.go
@@ -1,1 +1,30 @@
 package git
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestRevParseSingle(t *testing.T) {
+	repo := createTestRepo(t)
+	defer os.RemoveAll(repo.Workdir())
+
+	commitId, _ := seedTestRepo(t, repo)
+	fmt.Println(commitId)
+
+	revSpec, err := repo.RevParse("HEAD")
+	checkFatal(t, err)
+
+	checkObject(t, revSpec.From(), commitId)
+}
+
+func checkObject(t *testing.T, obj Object, id *Oid) {
+	if obj == nil {
+		t.Fatalf("bad object")
+	}
+
+	if !obj.Id().Equal(id) {
+		t.Fatalf("bad object, expected %s, got %s", id.String(), obj.Id().String())
+	}
+}
