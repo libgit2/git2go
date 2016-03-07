d3bd8903f8310baf5395d63aec1e3d517578156d
Carlos Martín Nieto <cmn@dwim.me>
2016-03-07 11:33:44 +0100

    Update libgit2 version to install on Travis to 24

--- script/install-libgit2.sh
+++ script/install-libgit2.sh
@@ -13,9 +13,10 @@ if [ "x$TRAVIS_BRANCH" = "xnext" ]; then
 fi
 
 cd "${HOME}"
-wget -O libgit2-0.23.1.tar.gz https://github.com/libgit2/libgit2/archive/v0.23.1.tar.gz
-tar -xzvf libgit2-0.23.1.tar.gz
-cd libgit2-0.23.1 && mkdir build && cd build
+LG2VER="0.24.0"
+wget -O libgit2-${LG2VER}.tar.gz https://github.com/libgit2/libgit2/archive/v${LG2VER}.tar.gz
+tar -xzvf libgit2-${LG2VER}.tar.gz
+cd libgit2-${LG2VER} && mkdir build && cd build
 cmake -DTHREADSAFE=ON -DBUILD_CLAR=OFF -DCMAKE_BUILD_TYPE="RelWithDebInfo" .. && make && sudo make install
 sudo ldconfig
 cd "${TRAVIS_BUILD_DIR}"
