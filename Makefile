5001942b59502155fc51b7d315b161c721ea1cf2
Carlos Martín Nieto <cmn@dwim.me>
2016-11-19 15:48:39 +0100

    Allow building statically via the "static" build tag

--- Makefile
+++ Makefile
@@ -9,3 +9,16 @@ test: build-libgit2
 
 install: build-libgit2
 	go install ./...
+
+build-libgit2:
+	./script/build-libgit2-static.sh
+
+static: build-libgit2
+	go run script/check-MakeGitError-thread-lock.go
+	go test --tags "static" ./...
+
+install-static: build-libgit2
+	go install --tags "static" ./...
+
+test-static: build-libgit2
+	go test --tags "static" ./...
---
+++ git_static.go
@@ -0,0 +1,17 @@
+// +build static
+
+package git
+
+/*
+#cgo CFLAGS: -I${SRCDIR}/vendor/libgit2/include
+#cgo LDFLAGS: -L${SRCDIR}/vendor/libgit2/build/ -lgit2
+#cgo windows LDFLAGS: -lwinhttp
+#cgo !windows pkg-config: --static ${SRCDIR}/vendor/libgit2/build/libgit2.pc
+#include <git2.h>
+
+#if LIBGIT2_VER_MAJOR != 0 || LIBGIT2_VER_MINOR != 25
+# error "Invalid libgit2 version; this git2go supports libgit2 v0.25"
+#endif
+
+*/
+import "C"
