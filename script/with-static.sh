98a017d5f738957c145d35be07c6cd1696322eba
Kyle L. Jensen <kljensen@gmail.com>
2017-01-12 13:32:08 -0500

    Set PKG_CONFIG_PATH so that libgit2.pc can be found when building statically linked against libgit2.

--- script/with-static.sh
+++ script/with-static.sh
@@ -4,6 +4,7 @@ set -ex
 
 export BUILD="$PWD/vendor/libgit2/build"
 export PCFILE="$BUILD/libgit2.pc"
+export PKG_CONFIG_PATH=$BUILD
 
 FLAGS=$(pkg-config --static --libs $PCFILE) || exit 1
 export CGO_LDFLAGS="$BUILD/libgit2.a -L$BUILD ${FLAGS}"
