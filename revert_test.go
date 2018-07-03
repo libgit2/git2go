0b5abad959dd5b001bb4f387baa6a6b9a165517f
Richard Burke <rich.g.burke@gmail.com>
2018-07-03 22:05:05 +0100

    Add revert functionality
    Closes #436
    
    (cherry picked from commit 30c3d0ffe2118376ccbf3ff5ea1676bd7442440d)

---
+++ revert_test.go
@@ -0,0 +1,77 @@
+package git
+
+import (
+	"testing"
+)
+
+const (
+	expectedRevertedReadmeContents = "foo\n"
+)
+
+func TestRevert(t *testing.T) {
+	t.Parallel()
+	repo := createTestRepo(t)
+	defer cleanupTestRepo(t, repo)
+
+	seedTestRepo(t, repo)
+	commitID, _ := updateReadme(t, repo, content)
+
+	commit, err := repo.LookupCommit(commitID)
+	checkFatal(t, err)
+
+	revertOptions, err := DefaultRevertOptions()
+	checkFatal(t, err)
+
+	err = repo.Revert(commit, &revertOptions)
+	checkFatal(t, err)
+
+	actualReadmeContents := readReadme(t, repo)
+
+	if actualReadmeContents != expectedRevertedReadmeContents {
+		t.Fatalf(`README has incorrect contents after revert. Expected: "%v", Actual: "%v"`,
+			expectedRevertedReadmeContents, actualReadmeContents)
+	}
+
+	state := repo.State()
+	if state != RepositoryStateRevert {
+		t.Fatalf("Incorrect repository state. Expected: %v, Actual: %v", RepositoryStateRevert, state)
+	}
+
+	err = repo.StateCleanup()
+	checkFatal(t, err)
+
+	state = repo.State()
+	if state != RepositoryStateNone {
+		t.Fatalf("Incorrect repository state. Expected: %v, Actual: %v", RepositoryStateNone, state)
+	}
+}
+
+func TestRevertCommit(t *testing.T) {
+	t.Parallel()
+	repo := createTestRepo(t)
+	defer cleanupTestRepo(t, repo)
+
+	seedTestRepo(t, repo)
+	commitID, _ := updateReadme(t, repo, content)
+
+	commit, err := repo.LookupCommit(commitID)
+	checkFatal(t, err)
+
+	revertOptions, err := DefaultRevertOptions()
+	checkFatal(t, err)
+
+	index, err := repo.RevertCommit(commit, commit, 0, &revertOptions.MergeOpts)
+	checkFatal(t, err)
+	defer index.Free()
+
+	revertOptions.CheckoutOpts.Strategy = CheckoutSafe
+	err = repo.CheckoutIndex(index, &revertOptions.CheckoutOpts)
+	checkFatal(t, err)
+
+	actualReadmeContents := readReadme(t, repo)
+
+	if actualReadmeContents != expectedRevertedReadmeContents {
+		t.Fatalf(`README has incorrect contents after revert. Expected: "%v", Actual: "%v"`,
+			expectedRevertedReadmeContents, actualReadmeContents)
+	}
+}
