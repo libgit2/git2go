3bec9c4db444714a9bcfcf1f332d5de4c0a88683
Dhruva Krishnamurthy <dhruvakm@gmail.com>
2020-01-21 08:24:07 -0800

    Fix libgit2 version dependency for dynamic linking

--- git_dynamic.go
+++ git_dynamic.go
@@ -6,8 +6,8 @@ package git
 #include <git2.h>
 #cgo pkg-config: libgit2
 
-#if LIBGIT2_VER_MAJOR != 0 || LIBGIT2_VER_MINOR != 27
-# error "Invalid libgit2 version; this git2go supports libgit2 v0.27"
+#if LIBGIT2_VER_MAJOR != 0 || LIBGIT2_VER_MINOR != 28
+# error "Invalid libgit2 version; this git2go supports libgit2 v0.28"
 #endif
 
 */
