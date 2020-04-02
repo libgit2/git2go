67c2ff3eb2f252cacf254ddd7889e2085ff312d2
lhchavez <lhchavez@lhchavez.com>
2020-04-02 07:42:29 -0700

    Uprev vendor/libgit2 to v1.0
    This uprevs libgit2 to the latest and greatest.

--- git_dynamic.go
+++ git_dynamic.go
@@ -6,8 +6,8 @@ package git
 #include <git2.h>
 #cgo pkg-config: libgit2
 
-#if LIBGIT2_VER_MAJOR != 0 || LIBGIT2_VER_MINOR != 99
-# error "Invalid libgit2 version; this git2go supports libgit2 v0.99"
+#if LIBGIT2_VER_MAJOR != 1 || LIBGIT2_VER_MINOR != 0
+# error "Invalid libgit2 version; this git2go supports libgit2 v1.0"
 #endif
 
 */
