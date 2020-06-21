7b1b0145e3da8257d7022d268625311ecd459e9d
lhchavez <lhchavez@lhchavez.com>
2020-06-21 15:40:52 -0700

    Revamp the ways in which the library can be built (#621)
    This change allows to link the system version of libgit2 statically.
    Since `-tags static` is already used for the bundled version of the
    library and to avoid breaking old workflows, `-tags
    static,system_libgit2` is now used to select that.
    
    This means that the valid combinations are:
    
    | Flag                          | Effect                                        |
    |-------------------------------|-----------------------------------------------|
    | _No flags_                    | Dynamically-linked against the system libgit2 |
    | `-tags static,system_libgit2` | Statically-linked against the system libgit2  |
    | `-tags static`                | Statically-linked against the bundled libgit2 |
    
    Note that there is no way to express dynamically linking against the
    bundled libgit2 because that makes very little sense, since the binaries
    wouldn't be able to be distributed. If that's still desired, the
    `PKG_CONFIG_PATH` environment variable can set before building the code.
    [`Makefile`](https://github.com/libgit2/git2go/blob/master/Makefile) has
    an example of how it is used in the CI.
    
    (cherry picked from commit 20a55cdf92f4ad6af4110861811a7076056cdf36)

--- git_bundled_static.go
+++ git_bundled_static.go
@@ -1,4 +1,4 @@
-// +build static
+// +build static,!system_libgit2
 
 package git
 
@@ -6,11 +6,11 @@ package git
 #cgo windows CFLAGS: -I${SRCDIR}/static-build/install/include/
 #cgo windows LDFLAGS: -L${SRCDIR}/static-build/install/lib/ -lgit2 -lwinhttp
 #cgo !windows pkg-config: --static ${SRCDIR}/static-build/install/lib/pkgconfig/libgit2.pc
+#cgo CFLAGS: -DLIBGIT2_STATIC
 #include <git2.h>
 
 #if LIBGIT2_VER_MAJOR != 0 || LIBGIT2_VER_MINOR != 28
 # error "Invalid libgit2 version; this git2go supports libgit2 v0.28"
 #endif
-
 */
 import "C"
--- git_system_dynamic.go
+++ git_system_dynamic.go
@@ -3,12 +3,12 @@
 package git
 
 /*
-#include <git2.h>
 #cgo pkg-config: libgit2
+#cgo CFLAGS: -DLIBGIT2_DYNAMIC
+#include <git2.h>
 
 #if LIBGIT2_VER_MAJOR != 0 || LIBGIT2_VER_MINOR != 28
 # error "Invalid libgit2 version; this git2go supports libgit2 v0.28"
 #endif
-
 */
 import "C"
