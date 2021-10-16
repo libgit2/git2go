235e9c0521458c9384ba4d5b2f0e64cbc36f1b19
lhchavez <lhchavez@lhchavez.com>
2021-10-16 04:44:39 -0700

    Declare forward-compatibility with libgit2 v1.3.0  #minor
    This commit marks this (Golang) version of git2go as being compatible
    with libgit2 v1.3.0.

--- Build_system_static.go
+++ Build_system_static.go
@@ -7,8 +7,8 @@ package git
 #cgo CFLAGS: -DLIBGIT2_STATIC
 #include <git2.h>
 
-#if LIBGIT2_VER_MAJOR != 1 || LIBGIT2_VER_MINOR < 1 || LIBGIT2_VER_MINOR > 2
-# error "Invalid libgit2 version; this git2go supports libgit2 between v1.1.0 and v1.2.0"
+#if LIBGIT2_VER_MAJOR != 1 || LIBGIT2_VER_MINOR < 1 || LIBGIT2_VER_MINOR > 3
+# error "Invalid libgit2 version; this git2go supports libgit2 between v1.1.0 and v1.3.0"
 #endif
 */
 import "C"
